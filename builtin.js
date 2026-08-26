2d5dc288f98e882be443e8321319f6c4f6e0ef8d
kuriarobert41-debug <kuriarobert41@gmail.com>
2026-08-26 14:41:03 +0300

    Add file reading and writing functionality

--- builtin.js
+++ builtin.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const content = fs.readFileSync('hello.js', 'utf-8');
+console.log(content);
 
+fs.readFile('hello.js', 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
+fs.writeFileSync('output.txt', 'Hello, World!');
+
+const path = require('path');
+console.log(path.join(__dirname, 'files', 'data.json'))
+console.log(path.extname('photo.jpg'));
